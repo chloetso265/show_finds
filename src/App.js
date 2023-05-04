a822799b5d227fbc92649bf7bbdfa2d35f72a64c
Chloe Tso <chloetso265@gmail.com>
2023-05-04 13:22:28 -0400

    site template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Show Finds</h1>
+      <h2>Home</h2>
+      <h2>Discover</h2>
+      <h2>Recommended</h2>
     </div>
   );
 }
