75c3705bb0a49eff3ec935e5a1c5e8bd0de87d09
Chloe Tso <chloetso265@gmail.com>
2023-05-07 15:58:42 -0400

    start card layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        {/* <Routes>
-            <Route path="/" element={<Warehouses />} />
-            <Route path="/edit" element={<EditWarehouse />} />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          {/* <Route path="/edit" element={<EditWarehouse />} />
             <Route path="/:id" element={<WarehouseDetails />} />
             <Route path="/addWarehouse" element={<AddWarehouse />} />
             <Route path="/deleteWarehouse" element={<DeleteWarehouse />} />
@@ -20,12 +21,9 @@ function App() {
             <Route path="/inventories" element={<Inventory />} />
             <Route path="/inventories/edit" element={<EditInventory />} />
             <Route path="/addNewItem" element={<AddItem />} />
-            <Route path="/inventories/:id" element={<ItemDetail />} />
-          </Routes>
-        <Footer /> */}
-        <h3>Home</h3>
-        <h3>Discover</h3>
-        <h3>Recommended</h3>
+  <Route path="/inventories/:id" element={<ItemDetail />} /> */}
+        </Routes>
+        {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,34 @@
+import "./Homepage.scss";
+import axios from "axios";
+
+function Homepage() {
+  return (
+    <section className="homepage">
+      <article>
+        <p>Filter Section</p>
+        <h4>Home</h4>
+        <h4>Discover</h4>
+        <h4>Recommended</h4>
+      </article>
+      <article className="homepage__main">
+        {/* <Card /> */}
+        <h2>Trending Now</h2>
+        <div className="homepage__shows">
+          <div>
+            <p>AUG 26</p>
+            <p>Sat - 8:00pm</p>
+          </div>
+          <div>
+            <p>Barclays Center - Brooklyn, NY</p>
+            <p>Chance The Rapper: Acid Rap 10 Year Anniversary Show</p>
+          </div>
+          <div>
+            <span>See Tickets</span>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default Homepage;
